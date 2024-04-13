d451da6ae973d27fc397f4e710beff1147b4a266
Sonu Kumar Yadav <yadavsonukumar036@gmail.com>
2024-04-13 21:54:59 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import Todo from './components/Todo';
 
+
 const App = () => {
   return (
-    <div className="flex items-center justify-center w-screen h-screen bg-gradient-to-b from-blue-300 to-blue-500">
+    <div style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '100vw',
+      height: '100vh',
+      backgroundImage: `url(${"b10.avif"})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat'
+    }}>
       <Todo />
     </div>
   );
